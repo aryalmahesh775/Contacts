59ab22b70f6c219c4688ff141ded5744e556dacb
aryalmahesh775 <aryalmahesh775@gmail.com>
2020-04-01 10:10:36 +0545

    Only backend

--- routes/users.js
+++ routes/users.js
@@ -50,24 +50,6 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-
-      const payload = {
-        user: {
-          id: user.id
-        }
-      };
-
-      jwt.sign(
-        payload,
-        config.get('jwtSecret'),
-        {
-          expiresIn: 36000000
-        },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
